38d130b6147cadcdee11bd31176a89478a79bdf6
kameldanial <kamelmikhaelkamel@gmail.com>
2025-06-08 23:33:53 +0300

    Add Strongly Typed IDs

---
+++ Src/Core/Domain/Orders/LineItem.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Products;
+using SharedKernal.Primitives;
+
+namespace Domain.Orders;
+
+public class LineItem
+{
+    internal LineItem(LineItemId id
+        , OrderId orderId
+        , ProductId productId
+        , Money price
+        , int quantity)
+        => (Id, OrderId, ProductId, Price, Quantity) = (id, orderId, productId, price, quantity);
+
+    public LineItemId Id { get; private set; }
+
+    public OrderId OrderId { get; private set; }
+
+    public ProductId ProductId { get; private set; }
+
+    public Money Price { get; private set; }
+
+    public int Quantity { get; set; }
+}
---
+++ Src/Core/Domain/Orders/Order.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Products;
+using Domain.Users;
+using SharedKernal.Primitives;
+
+namespace Domain.Orders;
+
+public class Order
+{
+    private Order()
+    { }
+
+    public OrderId Id { get; private set; }
+
+    public UserId UserId { get; private set; }
+
+    private readonly HashSet<LineItem> _lineItems = new();
+
+    public IReadOnlySet<LineItem> LineItems => _lineItems;
+
+    public void Add(ProductId productId, Money price, int quantity)
+        => _lineItems.Add(new(
+                new(Guid.NewGuid()), 
+                Id, 
+                productId, 
+                price, 
+                quantity)
+            );
+
+    public static Order Create(UserId userId) => new() 
+        {
+            Id = new(Guid.NewGuid()),
+            UserId = userId 
+        };
+}
